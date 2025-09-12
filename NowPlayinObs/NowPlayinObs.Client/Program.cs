1dc82b78aa9e0a6390e6ba339bd65565172e00c2
Florian Kunz <kunz.florian@gmx.de>
2025-09-12 11:48:10 +0200

    basic image n carousel added

--- NowPlayinObs/NowPlayinObs.Client/Program.cs
+++ NowPlayinObs/NowPlayinObs.Client/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddBlazorBootstrap();
+
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
